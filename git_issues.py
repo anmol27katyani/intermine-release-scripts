2733f272625f041e3500158679a979d00637850a
octamois <anmolmishra54@outlook.com>
2019-12-30 23:26:07 +0530

    Linting for git_issues.py

--- git_issues.py
+++ git_issues.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import requests
-import json
 
-issues = {}
 
-response = requests.get('https://api.github.com/repos/intermine/intermine/issues?direction=asc&state=all&milestone=48&per_page=1000')
+ISSUES = {}
+API_URL = 'https://api.github.com/'
+URL1 = API_URL+'repos/intermine/intermine/issues?direction=asc&state=all&milestone=48&per_page=1000'
+# For improving the code quality
+RESPONSE = requests.get(URL1)
 
-issues = response.json()
-for issue in issues:
-	if 'pull_request' not in issue:		
-		print("#" + str(issue['number']) + " - " + str(issue['title']))
+ISSUES = RESPONSE.json()
+for issue in ISSUES:
+    if 'pull_request' not in issue:
+        print("#" + str(ISSUES['number']) + " - " + str(ISSUES['title']))
 
 
 print("\n --------------------- \n")
-                
-milestones = {}
 
-response = requests.get('https://api.github.com/repos/intermine/intermine/milestones')
+MILESTONES = {}
 
-milestones = response.json()
-for milestone in milestones:
-        print("#" + str(milestone['number']) + " - " + str(milestone['title']))
+RESPONSE = requests.get('https://api.github.com/repos/intermine/intermine/milestones')
+
+MILESTONES = RESPONSE.json()
+for milestone in MILESTONES:
+    print("#" + str(MILESTONES['number']) + " - " + str(MILESTONES['title']))
 
 
 # https://developer.github.com/v3/issues/
-
